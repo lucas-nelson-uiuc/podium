368a5372164f25c2ebfe8b9efe02a99e6de71cf2
lucas-nelson-uiuc <lucas.nelson.contacts@gmail.com>
2025-03-09 19:16:48 -0500

    add dataframe converter

--- src/podium_lib/model/model.py
+++ src/podium_lib/model/model.py
@@ -121,6 +121,7 @@ class Model:
     @classmethod
     def validate(cls, data: nw.DataFrame) -> nw.DataFrame:
         """Run all provided validations against a DataFrame."""
+        data = nw.from_native(data)
         for name, field in cls.workflow():
             if field.validator:
                 try:
