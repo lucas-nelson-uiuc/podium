c653b4fb21c4202135264549a2192857618b2c73
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2025-03-16 13:04:35 -0500

    update typehints

--- src/podium_lib/utils.py
+++ src/podium_lib/utils.py
@@ -2,8 +2,8 @@ import narwhals as nw
 from narwhals.typing import IntoExpr
 
 
-def _as_expr(column: str | nw.Expr) -> IntoExpr:
+def _as_expr(column: str | IntoExpr) -> IntoExpr:
     """Coerce column-like object to expression."""
-    if not isinstance(column, nw.Expr):
+    if not isinstance(column, IntoExpr):
         column = nw.col(column)
     return column
