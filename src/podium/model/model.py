d68d71039381c4bd0243bc5b7bf8939b968da3d7
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2025-03-16 12:47:34 -0500

    add testing module

--- src/podium/model/model.py
+++ src/podium/model/model.py
@@ -1,81 +1,20 @@
-from typing import Optional
+from typing import Optional, Callable
 
 import narwhals as nw
 
 from podium.model.field import Field
 
 
-# @dataclass
-# class Model:
-#     """Podium Model class."""
-
-#     @classmethod
-#     def fields(cls, criteria: Callable = None) -> Sequence[Field]:
-#         """Extract fields from class with optional criteria."""
-#         fields = [getattr(cls, model_field) for model_field in cls.__dataclass_fields__]
-#         if criteria is None:
-#             return fields
-#         return list(filter(criteria, fields))
-
-#     @classmethod
-#     def document(
-#         cls, attributes: Sequence[str] = None, criteria: Callable = None
-#     ) -> Sequence[dict]:
-#         """Return documentation of class."""
-#         return [
-#             field.document(attributes=attributes)
-#             for field in cls.fields(criteria=criteria)
-#         ]
-
-#     @classmethod
-#     def __preprocess__(cls, data: DataFrameT) -> DataFrameT:
-#         """Process dataframe prior to conversion and validation."""
-#         return data
-
-#     @classmethod
-#     def __postprocess__(cls, data: DataFrameT) -> DataFrameT:
-#         """Process dataframe after applying conversion and valdiation."""
-#         return data
-
-#     @classmethod
-#     def convert(cls, data: DataFrameT) -> DataFrameT:
-#         """Apply field-level converters to all fields in model."""
-#         return data.with_columns(
-#             *(
-#                 podium_field.convert(column_exists=podium_field.name in data.columns)
-#                 for podium_field in cls.fields()
-#             )
-#         )
-
-#     # @classmethod
-#     # def validate(cls, data: DataFrameT) -> None:
-#     #     for podium_field in cls.fields():
-#     #         if podium_field.validator is None:
-#     #             pass
-#     #         column = nw.col(podium_field.alias)
-#     #         invalid_obs = data.select(column).filter(podium_field.validator(column))
-#     #         try:
-#     #             assert invalid_obs.is_empty()
-#     #             log_level = "SUCCESS"
-#     #             log_msg = "All observations passed the validator(s)."
-#     #         except AssertionError:
-#     #             log_level = "FAILURE"
-#     #             log_msg = "At least one observation failed the validator(s)."
-#     #             cls.__tracing__.errors[podium_field.name] = (
-#     #                 podium_field.validator,
-#     #                 invalid_obs,
-#     #             )
-#     #         finally:
-#     #             print(f"{podium_field.name} | {log_level}: {log_msg}")
-
-
 class Model:
     """Base class representing data-expression workflow."""
 
     @classmethod
-    def schema(cls) -> dict:
+    def schema(cls, predicate: Callable | None = None) -> dict:
         """Return class schema."""
-        return cls.__annotations__
+        schema = cls.__annotations__
+        if predicate is None:
+            return schema
+        return list(filter(predicate, schema))
 
     @classmethod
     def workflow(cls) -> dict:
